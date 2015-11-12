907f8f3fcb88474ed33f325f3223df4b9fbc72b9
MVDam <47767573t@iespoblenou.org>
2015-11-12 23:05:01 +0100

    creado arrayAdapter para lista ejemplo

--- app/src/main/java/com/primapp/movieappnuevo/MainActivityFragment.java
+++ app/src/main/java/com/primapp/movieappnuevo/MainActivityFragment.java
@@ -5,12 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
+    private ListView lvPelis:
+
 
     public MainActivityFragment() {
     }
@@ -20,9 +29,20 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        ListView lvPelis = (ListView)rootView.findViewById(R.id.lvPelis);
+        String[] peliEjemplo = {
+                "pelicula 1",
+                "pelicula 2",
+                "pelicula 3"
+        };
 
+        lvPelis = (ListView)rootView.findViewById(R.id.lvPelis);
+        items = new ArrayList<>(Arrays.asList(peliEjemplo));
+        adapter = new ArrayAdapter<>(
+                getContext()
+                ,R.layout.lv_peliculas
+                ,R.id.tvMovie
+                ,lvPelis
+        );
         return rootView;
-
     }
 }
